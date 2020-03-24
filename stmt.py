264f32d56b84e2e40756142175126a79d74f0218
gersondi <17ec13@aitdgoa.edu.in>
2020-03-24 16:58:27 +0530

    commit 2 made a phrase prog

--- stmt.py
+++ stmt.py
@@ -1,1 +1,7 @@
-print("apple")  
+fruit = input("An ______ a day keeps the doctor away\t")
+if fruit == ("apple")or("APPLE"):{
+                    
+                    print("correct answer")}
+    
+else : 
+    print("incorrect answer")
